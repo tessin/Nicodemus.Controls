5a6a512a69cdee253591fc94fe5f22a676a95878
Aleksey Shubin <aleksey@shubin.eu>
2016-10-04 21:46:28 +1000

    Selectable label control

---
+++ Nicodemus.Controls.Test/Nicodemus.Controls.Test.DesktopClient/Screens/TestDataItemsListDetail.lsml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using System.Windows.Controls;
+
+namespace LightSwitchApplication
+{
+    public partial class TestDataItemsListDetail
+    {
+        partial void TestDataItemsListDetail_Activated()
+        {
+            this.FindControl("TextField1").SetBinding(
+                TextBox.TextProperty,
+                "Value", System.Windows.Data.BindingMode.OneWay);
+        }
+    }
+}
---
+++ Nicodemus.Controls/SelectableLabel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Nicodemus.Controls
+{
+    public class SelectableLabel : TextBox
+    {
+        public SelectableLabel()
+        {
+            IsReadOnly = true;
+            DefaultStyleKey = typeof(SelectableLabel);
+        }
+    }
+}
