880c67b9b318f855d08c37c102fa64b879ba690f
John Leidegren <john@leidegren.se>
2017-02-01 18:45:33 +0100

    Fixed new line issues with XmlCodeEditorBox. See XmlTextEditor, optional XML validation enable by setting IsXmlValidationEnabled. Also, supports bi-directional databinding through LightSwitch ContentItem, see UserContentItemControl abstract base class.

---
+++ Nicodemus.Controls/Common/UserContentItemControl.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.LightSwitch.Presentation.Implementation;
+using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Nicodemus.Controls.Common
+{
+    public abstract class UserContentItemControl : UserControl
+    {
+        private readonly PropertyChangedEventHandler _contentItemPropertyChanged;
+
+        public ContentItem ContentItem { get; private set; }
+
+        public UserContentItemControl()
+        {
+            Loaded += (sender, e) =>
+            {
+                OnLoaded();
+            };
+
+            DataContextChanged += (sender, e) =>
+            {
+                for (var oldContentItem = e.OldValue as ContentItem; oldContentItem != null;)
+                {
+                    if (!(ContentItem == oldContentItem))
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    oldContentItem.PropertyChanged -= _contentItemPropertyChanged;
+                    OnContentItemUnsubscribed(oldContentItem);
+                    ContentItem = null;
+                    break;
+                }
+                for (var newContentItem = e.NewValue as ContentItem; newContentItem != null;)
+                {
+                    if (!(ContentItem == null))
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    newContentItem.PropertyChanged += _contentItemPropertyChanged;
+                    ContentItem = newContentItem;
+                    OnContentItemSubscribed(newContentItem);
+                    break;
+                }
+            };
+
+            _contentItemPropertyChanged = new PropertyChangedEventHandler((sender, e) =>
+            {
+                switch (e.PropertyName)
+                {
+                    case "Value":
+                        var contentItem = (ContentItem)sender;
+                        OnContentItemValueChanged(contentItem, contentItem.Value);
+                        break;
+                }
+            });
+
+            Unloaded += (sender, e) =>
+            {
+                var contentItem = ContentItem;
+                if (contentItem != null)
+                {
+                    contentItem.PropertyChanged -= _contentItemPropertyChanged;
+                }
+                OnUnloaded();
+            };
+        }
+
+        protected abstract void OnLoaded();
+
+        protected abstract void OnContentItemSubscribed(ContentItem contentItem);
+
+        protected abstract void OnContentItemValueChanged(ContentItem contentItem, object value);
+
+        protected abstract void OnContentItemUnsubscribed(ContentItem contentItem);
+
+        protected abstract void OnUnloaded();
+    }
+}
---
+++ Nicodemus.Controls/Editors/XmlTextEditor.cs
@@ -0,0 +1,116 @@
+﻿using com.bodurov.SilverlightControls.XmlCodeEditor;
+using Microsoft.LightSwitch.Presentation.Implementation;
+using Nicodemus.Controls.Common;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Nicodemus.Controls.Editors
+{
+    public class XmlTextEditor : UserContentItemControl
+    {
+        private readonly RoutedEventHandler _lostFocus;
+        private readonly XmlCodeEditorBox _editor;
+
+        public bool IsXmlValidationEnabled { get; set; }
+
+        public XmlTextEditor()
+        {
+            _editor = new XmlCodeEditorBox();
+            _lostFocus = new RoutedEventHandler(LostFocusHandler);
+            Content = _editor;
+        }
+
+        private void LostFocusHandler(object sender, RoutedEventArgs e)
+        {
+            if (ContentItem != null)
+            {
+                var text = _editor.Text;
+
+                // validate XML, let the exception propagate
+                if (IsXmlValidationEnabled)
+                {
+                    XDocument.Load(new StringReader(text), LoadOptions.SetLineInfo);
+                }
+
+                var text2 = NormalizeLineEndings(ContentItem.Value as string);
+                if (text != text2) // update only if changed
+                {
+                    ContentItem.Value = text;
+                }
+            }
+        }
+
+        protected override void OnLoaded()
+        {
+            _editor.LostFocus += _lostFocus;
+        }
+
+        protected override void OnContentItemSubscribed(ContentItem contentItem)
+        {
+            SetEditorText(contentItem.Value as string);
+        }
+
+        protected override void OnContentItemValueChanged(ContentItem contentItem, object value)
+        {
+            SetEditorText(contentItem.Value as string);
+        }
+
+        protected override void OnContentItemUnsubscribed(ContentItem contentItem)
+        {
+            SetEditorText(null);
+        }
+
+        private void SetEditorText(string text)
+        {
+            // for some reason the text is stored with CR only line endings
+            // whatever the case is we fix that here
+            string text2 = NormalizeLineEndings(text);
+            _editor.Text = text2;
+        }
+
+        protected override void OnUnloaded()
+        {
+            _editor.LostFocus -= _lostFocus;
+        }
+
+        private static string NormalizeLineEndings(string text)
+        {
+            // todo: if no line ending normalization is necessary we don't need to rebuild the string
+            //       it is much cheaper just to check that than to actually do the conversion
+
+            var sb = new StringBuilder();
+
+            var buf = (text ?? string.Empty).ToCharArray();
+            for (int i = 0, len = buf.Length; i < len;)
+            {
+                var ch = buf[i];
+                switch (ch)
+                {
+                    case '\r':
+                        if (i + 1 < len && buf[i + 1] == '\n')
+                        {
+                            i++;
+                        }
+                        sb.Append('\r');
+                        sb.Append('\n');
+                        i++;
+                        break;
+                    case '\n':
+                        sb.Append('\r');
+                        sb.Append('\n');
+                        i++;
+                        break;
+                    default:
+                        sb.Append(ch);
+                        i++;
+                        break;
+                }
+            }
+
+            var text2 = sb.ToString();
+            return text2;
+        }
+    }
+}
